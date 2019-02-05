f8eb1ea8bd81a99c86ae2f940bd08002bb0e2d85
chamipon <zkucera@uwo.ca>
2019-02-05 17:38:34 -0500

    DFS and BFS working

--- search/search.py
+++ search/search.py
@@ -91,31 +91,27 @@ def depthFirstSearch(problem):
   print "Start's successors:", problem.getSuccessors(problem.getStartState())
   print "Start's successors:", problem.getSuccessors(problem.getSuccessors(problem.getStartState())[1][0])
   from game import Directions
-  from util import Stack
   
 
   n=Directions.NORTH
   s=Directions.SOUTH
   e=Directions.EAST
   w=Directions.WEST
-  frontier = Stack()
   explored = []
   solution = []
 
   def recursiveDFS(node):
     if problem.isGoalState(node):
       print node , "IS GOAL STATE"
-      print "RETURNING SOLUTION :", solution
       return solution
     else:
+      print node, "HAS BEEN SEARCHED" 
       explored.append(node) #We have visited the node
       successors = problem.getSuccessors(node) #Get the successors
       for x in successors:#For each successor
         if x[0] not in explored:#If we havent visited the node
-          frontier.push(x[0])#Add the node to the frontier
-          print node, "-->",x[0]
           done = recursiveDFS(x[0])#Recurse on the new node, set done equal to the output
-          if done is not None:#If done is not empty, ei: If at the end of the recursion we found a goal state, add the direction to the start of the solution list
+          if done is not None:#If done is not empty, ei: If at any point we found a goal state, add the direction to the start of the solution list
             if x[1] == "North":
               solution.insert(0,n)
             elif x[1] == "South":
@@ -124,28 +120,75 @@ def depthFirstSearch(problem):
               solution.insert(0,e)
             elif x[1] == "West":
               solution.insert(0,w)
-            return done
+            return done 
               
-  frontier.push(problem.getStartState())
-            
-
-  currentNode = frontier.pop()
+  currentNode = problem.getStartState()
   explored.append(currentNode)
-  #done = recursiveDFS(currentNode)
-  #print "Length: ", len(done)
-  #print "Done: " , done
-  #return done
-  successors = problem.getSuccessors(currentNode)
+  done = recursiveDFS(currentNode)
+  print "Length: ", len(done)
+  print "Done: " , done
+  return done
   
-  while successors is not None:
-    for x in successors:
-      if x not in explored:
-        explored.append(x)
-        frontier.push(x)
+  
 
   util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
+  from game import Directions
+  from util import Queue
+  
+
+  n=Directions.NORTH
+  s=Directions.SOUTH
+  e=Directions.EAST
+  w=Directions.WEST
+  explored = []
+  solution = []
+  prev = []
+  curr=[]
+  queue = Queue()
+
+  def BFS(node):
+    firstRun = True #If this is the first pass through the while loop
+    if problem.isGoalState(node): #if the inital node is the goal state, then pacman starts on the goal state
+      print "START STATE IS THE GOAL STATE"
+      return solution
+    while queue: #As long as there are still nodes to explore
+      currentNode = queue.pop() #Get the next node in line
+
+      if firstRun is True:#If this is our first run through the loop
+        explored.append(currentNode)#Add it to explored
+        firstRun = False
+        successors = problem.getSuccessors(currentNode)#Get the successors
+        for x in successors:#Add the successors to the list
+          if x[0] not in explored:
+            prev.append((currentNode,0)) # prev and curr are used to keep a child/parent relationship between nodes to allow backtracking the path when the goal is found
+            curr.append(x)               #
+            queue.push(x)
+      else: #If it isnt our first run
+        explored.append(currentNode[0])#Add the current node to explored
+        if problem.isGoalState(currentNode[0]):# If we found the goal state, return the solution
+          print currentNode[0], "IS GOAL STATE"
+          
+          solution.insert(0,currentNode[1])#Add the final step to the solution
+          while currentNode not in problem.getSuccessors(problem.getStartState()): #Until we reach the starting position
+            currentNode = prev[curr.index(currentNode)] #Set the current node to the parent of the current node
+            solution.insert(0,currentNode[1])#Add the instruction to the solution.
+          return solution
+
+        successors = problem.getSuccessors(currentNode[0]) #Get the successors to the current state
+        for x in successors:#Add each successor to the queue, add the parent node to prev, and the successors to curr
+          if x[0] not in explored:
+            prev.append(currentNode)
+            curr.append(x)
+            queue.push(x)
+
+  currentNode = problem.getStartState()
+  explored.append(currentNode)
+  queue.push(currentNode)
+  done = BFS(currentNode)
+  return done
+
   "Search the shallowest nodes in the search tree first. [p 81]"
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
